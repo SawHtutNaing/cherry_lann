25050d244c0086eedd613cc0d6e298ccde2b1871
SawHtutNaing <sawhtutnaing2004@gmail.com>
2025-05-06 23:52:00 +0630

    dsfsdf

--- app/BoostStatus.php
+++ app/BoostStatus.php
@@ -6,14 +6,14 @@ enum BoostStatus: int
 {
     case Charge = 1;
     case Refund = 2;
-    case Pending = 3 ;
-    
+    case Pending = 3;
+
     public function label(): string
     {
         return match ($this) {
             self::Charge => 'Charge',
             self::Refund => 'Refund',
-            self::Refund => 'Pending',
+            self::Pending => 'Pending',
         };
     }
 }
